3a4816892e1ef703796cfb5da57e097b4a5f275a
kobajun0219 <kobajunoasis@yahoo.co.jp>
2021-12-12 16:21:22 +0900

    add seed and factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'message' => $this->faker->realText(rand(10, 50)),
+            'river_id' => $this->faker->numberBetween(1, 3),
+            'user_id' => $this->faker->numberBetween(1, 3),
+            'user_name' => $this->faker->name(),
+            'latitude' => $this->faker->latitude(),
+            'longitude' => $this->faker->longitude(),
+        ];
+    }
+}
