6705e3ab2b7184dd271a201b42ab1df15ffe842b
kobajun0219 <kobajunoasis@yahoo.co.jp>
2021-12-29 01:07:01 +0900

    add twitter link

--- resources/views/myriver.blade.php
+++ resources/views/myriver.blade.php
@@ -14,6 +14,7 @@
 <div class="w-50 m-auto" style="padding-bottom: 18px;">
   <div><span style="font-size: 18px; color: #4D7298;">{{config('river.river')[$river_id]}}</span> 投稿一覧</div>
   <p>投稿数：{{$posts->count()}}</p>
+  <a href="https://twitter.com/search?q=%23{{config('river.river')[$river_id]}}&src=typed_query&f=live">ツイッターで{{config('river.river')[$river_id]}}</a>
 </div>
 @if (count($posts) > 0)
 <!-- テーブル本体 -->
