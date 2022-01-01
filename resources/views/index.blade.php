8a2b38607235be60217945913cbfce2456260eb3
kobajun0219 <kobajunoasis@yahoo.co.jp>
2022-01-02 03:25:12 +0900

    fix index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,18 @@
         @include('common.errors')
         <!-- バリデーションエラーの表示に使用-->
 
-        
+
         {{-- スクレイピング結果表示 --}}
         <div class="w-50 m-auto" style="padding-bottom: 18px;">
           <div>{{$title}}</div>
           <div>{{$info}}</div>
         </div>
 
+        @auth
         {{-- modal --}}
         @include('post')
         {{-- modal end--}}
+        @endif
 
     {{-- 一覧表示 --}}
     @if (count($posts) > 0)
